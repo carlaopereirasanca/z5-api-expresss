9f30141998d45ffef77d4d03ccf1c0ac5b9041d2
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2024-10-30 18:34:38 -0300

    rotas feitas na mao

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Hello World!",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+};
+
 const server = http.createServer(
 
     (req, res) => {
-
         res.writeHead(200, {"Content-Type": "text/plain" } );
-        res.end("Hello World!");
+        console.log(req.url)
+        res.end( rotas[req.url] );
     }
 );
 
@@ -17,5 +23,4 @@ server.listen( PORT,
     () => {
         console.log("Servidor ativo e aguardando requisições...");
     }
-
 );
